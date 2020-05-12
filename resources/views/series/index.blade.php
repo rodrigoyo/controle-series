3bbb7452eeb6905696b4968dfb39d2c2c9f78d29
Rodrigo Yoshioka <rodrigo.yoshioka@gmail.com>
2020-05-12 15:02:48 -0300

    chore: Implement foreach in blade

--- resources/views/series/index.blade.php
+++ resources/views/series/index.blade.php
@@ -8,8 +8,8 @@ Séries
 <a href="/series/criar" class="btn btn-dark mb-2">Adicionar</a>
 
 <ul class="list-group">
-    <?php foreach ($series as $serie) : ?>
-        <li class="list-group-item"><?= $serie ?></li>
-    <?php endforeach ?>
+    @foreach($series as $serie)
+    <li class="list-group-item"><?= $serie ?></li>
+    @endforeach
 </ul>
 @endsection
